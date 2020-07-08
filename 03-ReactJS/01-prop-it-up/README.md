a1e61dd51bbdccd28505c5fc38186d0b1ac352b7
IURII <lytvyn.139@gmail.com>
2020-07-08 14:06:31 -0500

    prop it up lab

---
+++ 03-ReactJS/01-prop-it-up/README.md
@@ -0,0 +1,13 @@
+# Prop It Up
+Create a component called PersonCard that accepts the following props: 
+
+* firstName
+* lastName
+* age
+* hairColor
+Then, display four PersonCard components that show four different people, using props.
+# Run
+```
+npm i
+npm start
+```
---
+++ 03-ReactJS/01-prop-it-up/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+        <PersonCard
+          lastName = {'Doe'} 
+          firstName = {'Jane'}
+          age = {45} 
+          hairColor = {'Black'} />
+        
+        <PersonCard
+          lastName = {'John'} 
+          firstName = {'Smith'}
+          age = {88} 
+          hairColor = {'Brown'} />
+
+        <PersonCard
+          lastName = {'Mallard'} 
+          firstName = {'Fillmore'}
+          age = {50} 
+          hairColor = {'Brown'} />
+        
+        <PersonCard
+          lastName = {'Maria'} 
+          firstName = {'Smith'}
+          age = {62} 
+          hairColor = {'Brown'} />
+
+    </div>
+  );
+}
+
+export default App;
